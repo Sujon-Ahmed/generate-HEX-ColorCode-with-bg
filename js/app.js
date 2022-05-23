5ac0450973a1bc2653d1b547e0a6db0bd5522d50
Sujon-Ahmed <sujonahmed5284@gmail.com>
2022-05-23 15:31:13 +0600

    copy clr code btn with toastr msg added

--- js/app.js
+++ js/app.js
@@ -5,10 +5,30 @@ function main() {
     let root = document.getElementById('root');
     let btn = document.getElementById('change-btn');
     let colorCode = document.getElementById('colorCode');
+    let copyBtn = document.getElementById('copy-btn');
     btn.addEventListener('click', function() {
         let bgColor = generateHEXColor();
         root.style.backgroundColor = bgColor;
         colorCode.value = bgColor;
+    });
+    copyBtn.addEventListener('click', function() {
+        window.navigator.clipboard.writeText(colorCode.value);
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 2000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.addEventListener('mouseenter', Swal.stopTimer)
+              toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+          })
+          
+          Toast.fire({
+            icon: 'success',
+            title: ` ${colorCode.value} Copied Successfully!`
+          })
     });
 }
 
